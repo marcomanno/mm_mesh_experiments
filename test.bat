43990ca1c9e09a5cbdef537a0a3dd45b647c7153
marco <marco@LAPTOP-UIPMH9S0>
2016-11-28 00:02:30 +0000

    Test run via bat file.

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
 setlocal
 cd %~dp0
 set path=%path;%C:\Users\marco\OneDrive\Documents\PROJECTS\ThirdParties\vtk_installRelease\bin
-"C:\Users\marco\OneDrive\Documents\PROJECTS\polytriagnulation\out\Unittests\Release\unittests.exe"
+call "C:\Users\marco\OneDrive\Documents\PROJECTS\polytriagnulation\out\Unittests\Release\unittests.exe"
+pause
