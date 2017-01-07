c5d19b4b70cfed13b3634c8ef7e08a95a48a9afa
marco <marco@LAPTOP-UIPMH9S0>
2017-01-07 22:01:57 +0000

    use of kdtree in all boolean intersections.

--- main/src/Boolean/edge_vertex.cc
+++ main/src/Boolean/edge_vertex.cc
@@ -1,9 +1,11 @@
 
-#include "Boolean/priv.hh"
-#include "Utils/statistics.hh"
+#include "priv.hh"
+#include "Geo/entity.hh"
+#include "Geo/kdtree.hh"
 #include "Topology/iterator.hh"
 #include "Topology/split.hh"
-#include "Geo/entity.hh"
+#include "Utils/statistics.hh"
+
 
 #include <set>
 
@@ -25,43 +27,52 @@ bool EdgesVersusVertices::intersect(
   Topo::Iterator<Topo::Type::BODY, Topo::Type::VERTEX>& _vert_it,
   Topo::Iterator<Topo::Type::BODY, Topo::Type::EDGE>& _ed_it)
 {
-  for (size_t i = 0; i < _ed_it.size(); ++i)
+  Geo::KdTree<Topo::Wrap<Topo::Type::EDGE>> kdtree_e;
+  Geo::KdTree<Topo::Wrap<Topo::Type::VERTEX>> kdtree_v;
+
+  kdtree_e.insert(_ed_it.begin(), _ed_it.end());
+  kdtree_v.insert(_vert_it.begin(), _vert_it.end());
+  kdtree_e.compute();
+  kdtree_v.compute();
+
+  std::vector<std::array<size_t, 2>> couples =
+    Geo::find_kdtree_couples<Topo::Wrap<Topo::Type::EDGE>, Topo::Wrap<Topo::Type::VERTEX>>(
+      kdtree_e, kdtree_v);
+
+  for (const auto& couple : couples)
   {
-    Topo::Wrap<Topo::Type::EDGE> edge = _ed_it.get(i);
-    Topo::Iterator<Topo::Type::EDGE, Topo::Type::VERTEX> ev;
-    ev.reset(edge);
-    for (size_t j = 0; j < _vert_it.size(); ++j)
+    Topo::Wrap<Topo::Type::EDGE> edge = kdtree_e[couple[0]];
+    Topo::Iterator<Topo::Type::EDGE, Topo::Type::VERTEX> ev(edge);
+
+    Topo::Split<Topo::Type::EDGE>::Info spli;
+    spli.vert_ = kdtree_v[couple[1]];
+
+    bool found = false;
+    for (auto k = ev.size(); k-- > 0; )
     {
-      Topo::Split<Topo::Type::EDGE>::Info spli;
-      spli.vert_ = _vert_it.get(j);
-
-      bool found = false;
-      for (auto k = ev.size(); k-- > 0; )
+      if (ev.get(k) == spli.vert_)
       {
-        if (ev.get(k) == spli.vert_)
-        {
-          found = true;
-          break;
-        }
+        found = true;
+        break;
       }
-      if (found)
-        continue;  // The vertex is already on the edge.
-
-      Geo::Point pt;
-      spli.vert_->geom(pt);
-      Geo::Segment seg;
-      edge->geom(seg);
-      Geo::closest_point(seg, pt, &spli.clsst_pt_, &spli.t_, &spli.dist_);
-      Utils::FindMax<double> max_tol(spli.vert_->tolerance());
-      max_tol.add(edge->tolerance());
-      if (spli.dist_ > max_tol())
-        continue; // Vertex is too far.
-
-      auto it = ed_splt_set_.lower_bound(edge);
-      if (it == ed_splt_set_.end() || *it != edge)
-        it = ed_splt_set_.emplace_hint(it, edge);
-      it->add_point(spli);
     }
+    if (found)
+      continue;  // The vertex is already on the edge.
+
+    Geo::Point pt;
+    spli.vert_->geom(pt);
+    Geo::Segment seg;
+    edge->geom(seg);
+    Geo::closest_point(seg, pt, &spli.clsst_pt_, &spli.t_, &spli.dist_);
+    Utils::FindMax<double> max_tol(spli.vert_->tolerance());
+    max_tol.add(edge->tolerance());
+    if (spli.dist_ > max_tol())
+      continue; // Vertex is too far.
+
+    auto it = ed_splt_set_.lower_bound(edge);
+    if (it == ed_splt_set_.end() || *it != edge)
+      it = ed_splt_set_.emplace_hint(it, edge);
+    it->add_point(spli);
   }
   return true;
 }
--- main/src/Boolean/face_vertex.cc
+++ main/src/Boolean/face_vertex.cc
@@ -1,5 +1,6 @@
 
 #include "face_intersections.hh"
+#include "Geo/kdtree.hh"
 #include "Geo/pow.hh"
 
 #include <set>
@@ -10,24 +11,35 @@ bool FaceVersus::vertex_intersect(
   Topo::Iterator<Topo::Type::BODY, Topo::Type::FACE>& _face_it,
   Topo::Iterator<Topo::Type::BODY, Topo::Type::VERTEX>& _vert_it)
 {
-  for (auto& face : _face_it)
+  Geo::KdTree<Topo::Wrap<Topo::Type::FACE>> kdtree_f;
+  Geo::KdTree<Topo::Wrap<Topo::Type::VERTEX>> kdtree_v;
+
+  kdtree_f.insert(_face_it.begin(), _face_it.end());
+  kdtree_v.insert(_vert_it.begin(), _vert_it.end());
+  kdtree_f.compute();
+  kdtree_v.compute();
+
+  std::vector<std::array<size_t, 2>> couples =
+    Geo::find_kdtree_couples<Topo::Wrap<Topo::Type::FACE>, Topo::Wrap<Topo::Type::VERTEX>>(
+      kdtree_f, kdtree_v);
+
+  for (auto& couple : couples)
   {
+    const auto& face = kdtree_f[couple[0]];
     Topo::Iterator<Topo::Type::FACE, Topo::Type::VERTEX> fv_it(face);
     std::set<Topo::Wrap<Topo::Type::VERTEX>> face_verts(fv_it.begin(), fv_it.end());
     auto& face_info = face_geom(face);
-    for (auto& vert : _vert_it)
-    {
-      if (face_verts.find(vert) != face_verts.end())
-        continue;
-      Geo::Point pt, clsst_pt;
-      double dist_sq;
-      vert->geom(pt);
-      if (!closest_point(*face_info.poly_face_, pt, &clsst_pt, &dist_sq))
-        continue;
-      if (dist_sq > Geo::sq(vert->tolerance()))
-        continue;
-      face_info.new_vert_list_.push_back(vert);
-    }
+    const auto& vert = kdtree_v[couple[1]];
+    if (face_verts.find(vert) != face_verts.end())
+      continue;
+    Geo::Point pt, clsst_pt;
+    double dist_sq;
+    vert->geom(pt);
+    if (!closest_point(*face_info.poly_face_, pt, &clsst_pt, &dist_sq))
+      continue;
+    if (dist_sq > Geo::sq(vert->tolerance()))
+      continue;
+    face_info.new_vert_list_.push_back(vert);
   }
   return true;
 }
