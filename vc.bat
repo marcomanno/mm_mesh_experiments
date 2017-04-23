eec854aa836d2e3ed5058a754094835c39499222
marco <marco@LAPTOP-UIPMH9S0>
2017-04-23 18:00:10 +0100

    Visual studio 2017 update

--- vc.bat
+++ vc.bat
@@ -1,6 +1,7 @@
 setlocal
 cd %~dp0\out\src\UnitTest
 set path=%path;%C:\Users\marco\OneDrive\Documents\PROJECTS\ThirdParties\vtk_installRelease\bin
-%VS140COMNTOOLS%vsvars32.bat"
-start C:\Users\marco\OneDrive\Documents\PROJECTS\polytriagnulation\out\Project.sln
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+##%VS140COMNTOOLS%vsvars32.bat"
+devenv C:\Users\marco\OneDrive\Documents\PROJECTS\polytriagnulation\out\Project.sln
 exit 0
