5fa2d2398b53e35f6bb315b0f745f8a67486ed30
marco <marco@LAPTOP-UIPMH9S0>
2017-10-22 00:35:03 +0100

    Add containemnt tree

---
+++ main/src/UnitTest/containement_test.cc
@@ -0,0 +1,79 @@
+
+#include "catch/catch.hpp"
+#include "Utils/continement_tree.hh"
+
+#include <array>
+#include <queue>
+
+namespace
+{
+using Interval = std::array<double, 2>;
+int compare_intervals(const Interval& _a, const Interval& _b)
+{
+  if (_a == _b)
+    return -1;
+
+  if (_a[0] >= _b[0] && _a[1] <= _b[1])
+    return -1;
+  if (_b[0] >= _a[0] && _b[1] <= _a[1])
+    return 1;
+  return 0;
+}
+
+} // namespace
+
+TEST_CASE("containement_basic_001", "[Containement]")
+{
+  Utils::ContainementTree<Interval> cnt_interv(compare_intervals);
+  cnt_interv.add(Interval({0., 10.}));
+  cnt_interv.add(Interval({ 1., 5. }));
+  cnt_interv.add(Interval({ 6., 7. }));
+  cnt_interv.add(Interval({ 8., 9. }));
+  cnt_interv.add(Interval({ 2., 4. }));
+  cnt_interv.add(Interval({ 2.5, 3. }));
+  const auto& root = cnt_interv.root();
+  auto el = root->child();
+  for (int n = 3; --n >= 0; el = el->next())
+  {
+    if (static_cast<const Interval>(*el) == Interval({ 1., 5. }))
+      REQUIRE(el->child()->child()->child() == 0);
+    else
+      REQUIRE(el->child() == 0);
+  }
+  REQUIRE(el == 0);
+}
+
+TEST_CASE("containement_basic_002", "[Containement]")
+{
+  Utils::ContainementTree<Interval> cnt_interv(compare_intervals);
+  Interval intervals[] = {
+    { 0., 1. },
+    { -1., 1. },
+    { -2., 2. },
+    { 5., 10. },
+    { -3., 20. },
+    { 11., 13. }
+  };
+  for (const auto& i : intervals)
+    cnt_interv.add(i);
+  std::queue<const Utils::ContainementTree<Interval>::Element*> tree_queue;
+  tree_queue.push(cnt_interv.root());
+  auto nmbr = 0;
+  while (!tree_queue.empty())
+  {
+    const auto el = tree_queue.front();
+    tree_queue.pop();
+    ++nmbr;
+    if (el->child() != nullptr)
+    {
+      tree_queue.push(el->child());
+      REQUIRE(compare_intervals(*el, *el->child()) > 0);
+    }
+    if (el->next() != nullptr)
+    {
+      tree_queue.push(el->next());
+      REQUIRE(compare_intervals(*el, *el->next()) == 0);
+    }
+  }
+  REQUIRE(nmbr == std::size(intervals));
+}
---
+++ main/src/Utils/continement_tree.hh
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace Utils {
+
+// Implement a tree where any alement can have children and relatives.
+// It is useful to represent nested data, for example not intersecting loops.
+template <class ElemT>
+struct ContainementTree
+{
+  struct Element
+  {
+    friend struct ContainementTree;
+    Element(const ElemT& _elem) : elem_(_elem) { }
+    const Element* next() const { return next_.get(); }
+    const Element* child() const { return child_.get(); }
+    operator const ElemT()  const { return elem_; }
+  private:
+    ElemT elem_;
+    std::unique_ptr<Element> next_;
+    std::unique_ptr<Element> child_;
+  };
+  using CompareFunc = std::function<int(const ElemT&, const ElemT&)>;
+
+  ContainementTree(CompareFunc _cf) : cmp_func_(_cf) {}
+
+  const Element* root() const { return root_.get(); }
+
+  void add(const ElemT& _elem)
+  {
+    auto new_node = new Element(_elem);
+    std::unique_ptr<Element>* ins_el = &root_;
+    while (*ins_el)
+    {
+      auto res = cmp_func_(_elem, (*ins_el)->elem_);
+      if (res == 0)
+        ins_el = &(*ins_el)->next_;
+      else if (res < 0)
+        ins_el = &(*ins_el)->child_;
+      else // if (res > 0)
+      {
+        auto move_to = &new_node->child_;
+        bool first_iter = true;
+        while (*ins_el)
+        {
+          if (first_iter)
+            first_iter = false;
+          else
+            res = cmp_func_(_elem, (*ins_el)->elem_);
+          if (res <= 0)
+            ins_el = &(*ins_el)->next_;
+          else
+          {
+            std::swap(*ins_el, *move_to);
+            std::swap(*ins_el, (*move_to)->next_);
+            move_to = &(*move_to)->next_;
+          }
+        }
+      }
+    }
+    (*ins_el).reset(new_node);
+  }
+
+private:
+  std::unique_ptr<Element> root_;
+  CompareFunc cmp_func_;
+};
+
+}
