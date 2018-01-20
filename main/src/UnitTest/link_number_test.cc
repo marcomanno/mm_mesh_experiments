5bc27a81aa24e03f9f430c42c33d37e02923b2c4
marco <marco@LAPTOP-UIPMH9S0>
2018-01-20 15:55:41 +0000

    linking number test

--- main/src/UnitTest/link_number_test.cc
+++ main/src/UnitTest/link_number_test.cc
@@ -23,19 +23,38 @@ TEST_CASE("LnDisconnected", "[LINNUM]")
   REQUIRE(res == 0);
 }
 
-TEST_CASE("LnDisconnected1", "[LINNUM]")
+TEST_CASE("Link1", "[LINNUM]")
 {
-  std::vector<Geo::Vector<double, 3>> loop0 = {
+  std::vector<Geo::Vector<double, 3>> loop1 = {
     { 0,0,0 },
   { 0,0,1 },
   { 0,1,1 },
   { 0,1,0 }
   };
-  std::vector<Geo::Vector<double, 3>> loop1 = {
+  std::vector<Geo::Vector<double, 3>> loop0 = {
     { 0.5,0.5, -0.5 },
   { 0.5,0.5, 0.5 },
   { -0.5,0.5,0.5 },
   { -0.5, 0.5, -0.5 }
+  };
+  auto res = Geo::LinkingNumber::compute(loop0, loop1);
+  REQUIRE(res == 0);
+}
+
+TEST_CASE("Link1_a", "[LINNUM]")
+{
+  std::vector<Geo::Vector<double, 3>> loop1 = {
+    { 0,0,0 },
+  { 0,0,1 },
+  { 0,1,1 },
+  { 0,1,0 }
+  };
+  std::vector<Geo::Vector<double, 3>> loop0 = {
+    {  0.5, 0.5, -0.5 },
+    {  0.0001, 0.5,  0 },
+    {  0.5, 0.5,  0.5 },
+    { -0.5, 0.5,  0.5 },
+    { -0.5, 0.5, -0.5 }
   };
   auto res = Geo::LinkingNumber::compute(loop0, loop1);
   REQUIRE(res == 0);
